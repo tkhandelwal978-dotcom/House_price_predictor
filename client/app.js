0e8d2346a222808ac8fa1f7541347fc09fca9a35
Tanishq Khandelwal <tkhandelwal978@gmail.com>
2025-10-08 14:26:53 +0530

    Initial commit

---
+++ client/app.js
@@ -0,0 +1,66 @@
+function getBathValue() {
+    // Select all radio buttons with name "uiBathrooms"
+    var uiBathrooms = document.getElementsByName("uiBathrooms");
+    for (var i = 0; i < uiBathrooms.length; i++) {
+        if (uiBathrooms[i].checked) {
+            return parseInt(uiBathrooms[i].value);
+        }
+    }
+    return -1; // if none selected
+}
+
+function getBHKValue() {
+    // Select all radio buttons with name "uiBHK"
+    var uiBHK = document.getElementsByName("uiBHK");
+    for (var i = 0; i < uiBHK.length; i++) {
+        if (uiBHK[i].checked) {
+            return parseInt(uiBHK[i].value);
+        }
+    }
+    return -1; // if none selected
+}
+
+function onClickedEstimatePrice() {
+    console.log("Estimate price button clicked");
+
+    var sqft = document.getElementById("uiSqft");
+    var bhk = getBHKValue();
+    var bathrooms = getBathValue();
+    var locations = document.getElementById("uiLocations");
+    var estPrice = document.getElementById("uiEstimatedPrice");
+
+    var url = "http://127.0.0.1:5000/predict_home_price";
+
+    $.post(url, {
+        total_sqft: parseFloat(sqft.value),
+        bhk: bhk,
+        bath: bathrooms,
+        location: locations.value
+    }, function(data, status) {
+        console.log(data.estimated_price);
+        estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+        console.log(status);
+    });
+}
+
+function onPageLoad() {
+    console.log("Document loaded");
+    var url = "http://127.0.0.1:5000/get_location_names";
+
+    $.get(url, function(data, status) {
+        console.log("Got response for get_location_names");
+
+        if (data) {
+            var locations = data.locations;
+            var uilocations = document.getElementById("uiLocations");
+            $('#uiLocations').empty();
+
+            for (var i in locations) {
+                var opt = new Option(locations[i]);
+                $('#uiLocations').append(opt);
+            }
+        }
+    });
+}
+
+window.onload = onPageLoad;
