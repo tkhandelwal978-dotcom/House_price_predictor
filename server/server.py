1077fbb3854e877ae159fce5a758d42b2b55f367
Tanishq Khandelwal <tkhandelwal978@gmail.com>
2025-10-08 16:35:53 +0530

    Move requirements.txt into server folder for Render deployment

--- server/server.py
+++ server/server.py
@@ -27,6 +27,6 @@ def predict_home_price():
     return response
 
 if __name__ == "__main__":
-    print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
-    app.run()
+    # print("Starting Python Flask Server For Home Price Prediction...")
+    # util.load_saved_artifacts()
+    app.run(host="0.0.0.0",port=5000)
